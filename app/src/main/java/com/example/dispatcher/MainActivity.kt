e2035b0dd5edc58f0149260e88a3d08d2480a7e1
Dolev Gabay <who@Dolev-Gabay.local>
2024-08-27 16:54:03 +0300

    first commit

---
+++ app/src/main/java/com/example/dispatcher/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.dispatcher
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
+
+
+
