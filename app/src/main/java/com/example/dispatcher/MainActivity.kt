476bfc72c207c6f9df3b9fdbc746a94c83523b17
Dolev-g <dolevg@moveo.co.il>
2024-08-28 11:29:33 +0300

    third development commit

--- app/src/main/java/com/example/dispatcher/MainActivity.kt
+++ app/src/main/java/com/example/dispatcher/MainActivity.kt
@@ -12,7 +12,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val a = 6
 
     }
 }
